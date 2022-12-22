ee7b43bda9bc31f0a967116563c4258f52a168a9
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-21 20:05:50 -0600

    added menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,11 @@ import '../Portfolio.css'
 export default function Menu (){
     return (
         <div>
-            <span></span>
+            <section className="menu">
+            <h2>CV</h2>
+            <h2>PROJECTS</h2>
+            <h2>VIDEO GAME REVIEWS</h2>
+            </section>
         </div>
     )
 }
