3782c277ef2e95d2e4299356e53291ce66cad701
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-21 20:22:52 -0600

    added info of projects to the project components

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,10 +1,14 @@
 import '../Portfolio.css'
+import cv from '../docs/CV_2022.pdf'
 
 export default function Cv (){
     return (
         <div>
-            <span></span>
-            
+            <section id="cv">
+            <h2>CV</h2>
+            <p>Here is a link to my CV</p>
+            <a href={cv} rel="noreferrer">Glo's 2022 CV</a>
+            </section>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,29 @@ import '../Portfolio.css'
 export default function Projects (){
     return (
         <div>
-            <span></span>
+            <section id="projects">
+            <h2>Projects</h2>
+            <p>These are websites I've designed and created from scratch (HTML, CSS, JS) for UT Bootcamp</p>
+                <ul className="links">
+                    <li id="trueque"><a href="https://the-sequelizers.herokuapp.com/" rel="noreferrer">Trueque</a></li>
+                    <li id="techblog"><a href="https://github.com/ggggglo/Tech_Blog" rel="noreferrer">Tech Blog</a></li>
+                    <li id="bored"><a href="https://catpolk.github.io/Bored-NO-MO-project-1/" rel="noreferrer">Bored NO-MO!</a></li>
+                    <li id="quiz"><a href="https://ggggglo.github.io/Impossible_Quiz/" rel="noreferrer">Impossible Quiz</a></li>
+                    <li id="weather"><a href="https://ggggglo.github.io/Weather_Dashboard/" rel="noreferrer">Weather'ed</a></li>
+                    <li id="organizer"><a href="https://ggggglo.github.io/Daily_Organizer/" rel="noreferrer">Daily Organizer</a></li>
+                </ul>
+                
+            <p>These are websites I've designed and created using Wordpress</p>
+         
+                <ul class="links">
+                    <li id="eva"><a href="https://www.evaclaessens.com/" rel="noreferrer">Artist Eva Claessens</a></li>
+                    <li id="rebrand"><a href="https://www.rebrandcr.com/" rel="noreferrer">Marketing Agency Rebrand</a></li>
+                    <li id="omina"><a href="https://www.ominafoundation.org" rel="noreferrer">NGO OMINA Foundation</a></li>
+                    <li id="huella"><a href="https://dejandohuellacr.org/" rel="noreferrer"> NGO Dejando Huella</a></li>
+                    <li id="somabacu"><a href="https://somabacu.com/" rel="noreferrer">Construction Company SOMABACU</a></li>
+                    <li id="ecostaff"><a href="https://www.ecostaffcr.com/" rel="noreferrer">Facility Management Company EcoStaff</a></li>   
+                </ul>
+        </section>
         </div>
     )
 }
