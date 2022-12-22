5a2cad1447cb14424ab329f93b7a0b3bca5e428f
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-21 19:52:41 -0600

    added intro html and fixed classes in nav

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,34 @@ import '../Portfolio.css'
 
 export default function Intro (){
     return (
-        <div>
-            <span></span>
-        </div>
+        <div className="about"> 
+        <section>
+
+        <img id="glo" src="../../public/images/glo-portrait.jpg" alt=" Glo, white, 28 years old, black hair wearing leather jacket"/>
+        <img id= "kylo" src="../public/images/kylo.jpg" alt="Kylo, a black, long haired, sharpei"/>
+        </section>
+
+        <section className="about-text">
+        <p id="textcenter">  This is me, Glo, and this is Kylo </p>
+        <br/>
+        <p id="textright">Kylo is a 2yo bear coat Shar Pei </p>
+        <br/>
+        <p className="textleft"> I am from San José, Costa Rica.</p>
+        <p className="textleft">I went to school for production design.</p>
+        <p className="textleft">Worked in different theaters and</p>
+        <p className="textleft">very cool places like Universal and Disney.</p>
+        <p className="textleft">As of the last couple of years I have been</p>
+        <p className="textleft">more involved in administrative roles.</p>
+        <p className="textleft">This year I enrolled in the UT Coding Bootcamp</p>
+        <p className="textleft">and will be working towards a career in coding.</p>
+        <br/>    
+        <p id="textright"> Kylo hasn't done much, but he's great</p>    
+        </section>
+
+        <section>
+        <img id= "kylo2" src="../public/images/kylo.jpg" alt="Kylo, a black, long haired, sharpei"/>
+        </section>
+</div>
+    
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,16 @@ export default function Nav (){
     const mailto = 'mailto:glorianagonzalez@rocketmail.com'
     return (
         <div>
-            <span class="header">
+            <span className="header">
             <h1>Glo's Portfolio</h1>
-            <h2>Test</h2>
             </span>
             <span>
-            <p class="contact">
+            <p className="contact">
             <a href= {mailto}>glorianagonzalez@rocketmail.com</a>
             </p>
-            <p class="contact">+512.696.5501</p>
-            <p class="contact">Austin, TX</p>
-            <p id="bday" class="contact">03/10/94</p>
+            <p className="contact">+512.696.5501</p>
+            <p className="contact">Austin, TX</p>
+            <p id="bday" className="contact">03/10/94</p>
             </span>
         </div>
     )
